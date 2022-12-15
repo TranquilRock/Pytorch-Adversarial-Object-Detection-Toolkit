28e7da3d9755efd15831cf44e4ddb4cb2a2498dc
TranquilRock <andy255060@gmail.com>
2022-12-15 18:49:12 +0800

    Successfully attacked on score, not good enough

--- attack/gradient_based.py
+++ attack/gradient_based.py
@@ -1,24 +1,80 @@
 """ Implements attack methods.
 Currently wrap advertorch for comparison.
 """
-from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Callable, Optional, Tuple
 
 import torch
 from advertorch import attacks
 from torch import nn
 
-
-class Attack(ABC):
-    """Abstract class for all attack methods."""
-
-    @abstractmethod
-    def __call__(self, x: Any, y: Any) -> Any:
-        pass
+from .attack import Attack, AttackWithBound
 
 
-class FGSM(Attack):
+class LinfPGD(Attack):
+    """Projected Gradient Descent implementation, now migrate to advertorch package."""
+
+    def __init__(
+        self,
+        model: nn.Module,
+        loss_fn: nn.Module,
+        eps: float,
+        nb_iter: int,
+        eps_iter: Optional[float] = None,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+        rand_init=True,
+    ) -> None:
+        super().__init__()
+        if eps_iter is None:
+            eps_iter = eps / nb_iter
+        self.adversary = attacks.LinfPGDAttack(
+            model,
+            loss_fn=loss_fn,
+            eps=eps,
+            nb_iter=nb_iter,
+            eps_iter=eps_iter,
+            rand_init=rand_init,
+            clip_min=clip_min,
+            clip_max=clip_max,
+            targeted=False,
+        )
+
+    @torch.enable_grad()
+    def __call__(
+        self,
+        x: torch.Tensor,
+        y: torch.Tensor,
+    ) -> torch.Tensor:
+        return self.adversary.perturb(x, y)
+
+
+class FGSM(LinfPGD):
     """Fast gradient signed method implementation."""
+
+    def __init__(
+        self,
+        model: nn.Module,
+        loss_fn: nn.Module,
+        eps: float,
+        nb_iter: int,
+        eps_iter: Optional[float] = None,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+    ) -> None:
+        super().__init__(
+            model,
+            loss_fn,
+            eps,
+            nb_iter,
+            eps_iter,
+            clip_min,
+            clip_max,
+            False,
+        )
+
+
+class LinfPGDWithBound(AttackWithBound):
+    """Projected Gradient Descent implementation, now migrate to advertorch package."""
 
     def __init__(
         self,
@@ -42,61 +98,24 @@ class FGSM(Attack):
         self.clip_max = clip_max
 
     @torch.enable_grad()
-    def __call__(self, x, y) -> Any:
+    def __call__(
+        self,
+        x: torch.Tensor,
+        y: torch.Tensor,
+        bound: Tuple[int, int, int, int],
+        collate_fn: Callable = lambda x: x,
+    ) -> torch.Tensor:
+        x1, y1, x2, y2 = bound
         x_adv = x.detach()
+        x_adv[:, :, x1:x2, y1:y2] += torch.zeros_like(x_adv).uniform_(-self.eps, self.eps)[:, :, x1:x2, y1:y2]
         for _ in range(self.nb_iter):
             x_adv.requires_grad_(True)
-            loss = self.loss_fn(self.model(x_adv), y)
+            loss = self.loss_fn(collate_fn(self.model(x_adv)), y)
             grad = torch.autograd.grad(loss, [x_adv])[0]
-            x_adv = x_adv.detach() + self.eps_iter * torch.sign(grad.detach())
-            x_adv = torch.max(torch.min(x_adv, x + self.eps_iter), x - self.eps_iter)
-            x_adv = torch.clamp(x_adv, self.clip_min, self.clip_max)
+            grad =  self.eps_iter * torch.sign(grad.detach())
+            x_adv = x_adv.detach()
+            x_adv[:, :, x1:x2, y1:y2] += grad[:, :, x1:x2, y1:y2]
+            x_adv = (x_adv.max(x - self.eps)).min(x + self.eps)
+            x_adv = torch.clamp(x_adv, 0, 1)
+
         return x_adv
-
-
-class LinfPGD(Attack):
-    """Projected Gradient Descent implementation, now migrate to advertorch package."""
-
-    def __init__(
-        self,
-        model: nn.Module,
-        loss_fn: nn.Module,
-        eps: float,
-        nb_iter: int,
-        eps_iter: Optional[float] = None,
-        clip_min: float = 0.0,
-        clip_max: float = 1.0,
-    ) -> None:
-        super().__init__()
-        if eps_iter is None:
-            eps_iter = eps / nb_iter
-        self.adversary = attacks.LinfPGDAttack(
-            model,
-            loss_fn=loss_fn,
-            eps=eps,
-            nb_iter=nb_iter,
-            eps_iter=eps_iter,
-            rand_init=True,
-            clip_min=clip_min,
-            clip_max=clip_max,
-            targeted=False,
-        )
-
-    @torch.enable_grad()
-    def __call__(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        return self.adversary.perturb(x, y)
-
-
-@DeprecationWarning  # This will raise Err since its not callable.
-@torch.enable_grad()
-def linf_pgd(model, x, y, loss_fn, epsilon, alpha, num_iter) -> torch.Tensor:
-    x_adv = x.detach()
-    x_adv += torch.zeros_like(x_adv).uniform_(-epsilon, epsilon)
-    for _ in range(num_iter):
-        x_adv.requires_grad_(True)
-        loss = loss_fn(model(x_adv), y)
-        grad = torch.autograd.grad(loss, [x_adv])[0]
-        x_adv = x_adv.detach() + alpha * torch.sign(grad.detach())
-        x_adv = (x_adv.max(x - epsilon)).min(x + epsilon)
-        x_adv = torch.clamp(x_adv, 0, 1)
-    return x_adv
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Tuple
 from torchvision.io.image import read_image
 from torchvision.models.detection import (
     fasterrcnn_resnet50_fpn_v2,
@@ -5,15 +6,83 @@ from torchvision.models.detection import (
 )
 from torchvision.utils import draw_bounding_boxes, save_image
 from util import LABEL_DICT
+from attack.gradient_based import LinfPGDWithBound
+import torch
+from torch import nn
 
-if __name__ == "__main__":
-    source_img = read_image("/home/ordinaryhuman/sample.png")
+ROOT = "/home/ordinaryhuman/Adversarial-Composition/assets"  # "./assets"
+DEVICE = torch.device("cuda:0")
+
+
+def get_image(path: str = f"{ROOT}/sample.png") -> Tuple[torch.Tensor, torch.Tensor]:
+    source_img = read_image(path=path)
     img = (source_img.clone() / 255).unsqueeze(0)
-    model = fasterrcnn_resnet50_fpn_v2(
-        weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT, box_score_thresh=0.9
+    return source_img.to(DEVICE), img.to(DEVICE)
+
+
+class SumScoreLoss(nn.Module):
+    def forward(self, x: List[torch.Tensor], y: torch.Tensor) -> torch.Tensor:
+        return -torch.concat(x).sum()  # Minimize -score -> Maximize score.
+
+
+def main() -> None:
+    source_img, img = get_image()
+    model = (
+        fasterrcnn_resnet50_fpn_v2(
+            weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT, box_score_thresh=0.5
+        )
+        .train()
+        # .eval()
+        .to(DEVICE)
     )
-    model.eval()
-    outputs = model(img)
+
+    truth = [
+        {
+            "boxes": torch.Tensor(
+                [
+                    [415.1139, 150.9288, 545.6686, 414.7146],
+                    [1.0540, 174.2773, 134.2004, 353.4934],
+                    [95.4936, 180.1695, 235.8846, 291.5413],
+                    [182.9897, 178.8918, 257.3154, 255.9231],
+                    [416.5106, 185.4812, 445.5121, 261.6208],
+                    [333.7903, 192.2578, 351.0151, 259.7337],
+                    [437.7644, 292.8659, 501.6620, 405.6678],
+                    [394.6197, 190.9600, 415.4427, 245.7377],
+                    [282.3032, 151.4133, 310.8425, 211.5469],
+                    [628.7678, 173.0963, 649.4948, 285.8481],
+                    [704.1951, 171.3914, 735.0613, 258.0855],
+                    [718.6891, 190.4808, 736.1738, 222.0931],
+                ],
+            ).to(DEVICE),
+            "labels": torch.Tensor(
+                [1, 3, 3, 3, 1, 1, 28, 1, 1, 1, 1, 31]
+            ).to(torch.int64).to(DEVICE),
+        }
+    ]
+    print(model(img, truth))  # , boxes = [0,0, 1,1]
+
+    attack = LinfPGDWithBound(
+        model=model,
+        loss_fn=SumScoreLoss(),
+        eps=1,
+        nb_iter=1000,
+        eps_iter=0.01,
+    )
+
+    attacked_img = attack(
+        x=img,
+        y=torch.zeros(1),
+        bound=(
+            60,
+            320,
+            110,
+            430,
+        ),
+        collate_fn=lambda outputs: [output["boxes"] for output in outputs],
+    )
+    save_image(attacked_img, f"{ROOT}/attacked.png")
+
+    outputs = model(attacked_img)
     for i, output in enumerate(outputs):
         boxes, labels, scores = output["boxes"], output["labels"], output["scores"]
         labels = [
@@ -21,5 +90,9 @@ if __name__ == "__main__":
             for label, score in zip(labels.tolist(), scores.tolist())
         ]
         result = draw_bounding_boxes(source_img, boxes.detach(), labels) / 255
-        save_image(result, f"/home/ordinaryhuman/result_{i}.png")
-    # (320, 60), (430, 110)
+        save_image(result, f"{ROOT}/attacked_result.png")
+    save_image((attacked_img - img) ** 2, f"{ROOT}/diff.png")
+
+
+if __name__ == "__main__":
+    main()
