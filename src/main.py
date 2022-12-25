84ff5c9619906a4e713b1a6273a0ca8841ca4838
Jan-Ting Du <ordinaryhuman@Latias.csie.org>
2022-12-25 10:28:10 +0800

    Finish experiments

--- src/main.py
+++ src/main.py
@@ -47,9 +47,104 @@ model = ssdlite320_mobilenet_v3_large(
 
 
 ASSETS_ROOT = "../assets"
-SAVE_ROOT = "../assets/bounded"
 DEVICE = torch.device("cuda:0")
 SAMPLE_BOUND = (60, 320, 110, 430)  # You can set a desired region to perform attack.
+SETTINGS = {
+    "bounded_targeted": {
+        "save_path": f"{ASSETS_ROOT}/experiments/bounded/targeted/",
+        "eps": 255 / 255,
+        "y": [
+            {
+                "boxes": torch.Tensor(
+                    [[330, 70, 420, 100]],
+                ).to(DEVICE),
+                "labels": torch.Tensor([1]).to(DEVICE).to(torch.int64),
+            }
+        ],
+        "bound": (60, 320, 110, 430),
+        "target_losses": [
+            "loss_classifier",
+            "loss_objectness",
+            "loss_box_reg",
+            "loss_rpn_box_reg",
+        ],
+    },
+    "bounded_untarget": {
+        "save_path": f"{ASSETS_ROOT}/experiments/bounded/untarget/",
+        "eps": 255 / 255,
+        "bound": (60, 320, 110, 430),
+        "target_losses": [
+            "loss_classifier",
+            "loss_objectness",
+            "loss_box_reg",
+            "loss_rpn_box_reg",
+        ],
+    },
+    "unbounded_targeted_box": {
+        "save_path": f"{ASSETS_ROOT}/experiments/unbounded/box/targeted",
+        "eps": 16 / 255,
+        "y": [
+            {
+                "boxes": torch.Tensor(
+                    [[400, 0, 563, 1024], [200, 200, 400, 400], [400, 400, 563, 1024]]
+                ).to(DEVICE),
+                "labels": torch.Tensor([24, 24, 24]).to(DEVICE).to(torch.int64),
+            }
+        ],
+        "target_losses": [
+            "loss_box_reg",
+            "loss_rpn_box_reg",
+        ],
+    },
+    "unbounded_targeted_cls": {
+        "save_path": f"{ASSETS_ROOT}/experiments/unbounded/cls/targeted",
+        "eps": 16 / 255,
+        "y": [
+            {
+                "boxes": torch.Tensor(
+                    [[400, 0, 563, 1024], [200, 200, 400, 400], [400, 400, 563, 1024]]
+                ).to(DEVICE),
+                "labels": torch.Tensor([24, 24, 24]).to(DEVICE).to(torch.int64),
+            }
+        ],
+        "target_losses": [
+            "loss_classifier",
+            "loss_objectness",
+        ],
+    },
+    "unbounded_untarget_box": {
+        "save_path": f"{ASSETS_ROOT}/experiments/unbounded/box/untarget",
+        "eps": 16 / 255,
+        "target_losses": [
+            "loss_box_reg",
+            "loss_rpn_box_reg",
+        ],
+    },
+    "unbounded_untarget_cls": {
+        "save_path": f"{ASSETS_ROOT}/experiments/unbounded/cls/untarget",
+        "eps": 16 / 255,
+        "target_losses": [
+            "loss_classifier",
+            "loss_objectness",
+        ],
+    },
+    "standard_targeted": {
+        "save_path": f"{ASSETS_ROOT}/experiments/standard/targeted",
+        "eps": 4 / 255,
+        "y": [
+            {
+                "boxes": torch.Tensor(
+                    [[400, 0, 563, 1024], [200, 200, 400, 400], [400, 400, 563, 1024]]
+                ).to(DEVICE),
+                "labels": torch.Tensor([24, 24, 24]).to(DEVICE).to(torch.int64),
+            }
+        ],
+    },
+    "standard_untarget": {
+        "save_path": f"{ASSETS_ROOT}/experiments/standard/untarget",
+        "eps": 4 / 255,
+    },
+}
 
 
 def get_image(
@@ -65,7 +160,14 @@ def get_image(
     return source_img.to(DEVICE), img.to(DEVICE)
 
 
-def main(model: nn.Module) -> None:
+def main(
+    model: nn.Module,
+    eps: float,
+    save_path: str,
+    y: Optional[Any] = None,
+    target_losses: Optional[List[str]] = None,
+    bound: Optional[Any] = None,
+) -> None:
     """Main entry for demo."""
     source_img, img = get_image()
 
@@ -76,21 +178,15 @@ def main(model: nn.Module) -> None:
 
     attack = ObjectDetectionLinfPGD(
         model=model,
-        eps=255 / 255,
-        nb_iter=1000,
+        eps=eps,
+        nb_iter=100,
     )
 
     attacked_img = attack(
         x=img,
-        y=[
-            {
-                "boxes": torch.Tensor(
-                    [[400, 0, 563, 1024], [200, 200, 400, 400], [400, 400, 563, 1024]]
-                ).to(DEVICE),
-                "labels": torch.Tensor([24, 24, 24]).to(DEVICE).to(torch.int64),
-            }
-        ],
-        bound=SAMPLE_BOUND,
+        y=y,
+        target_losses=target_losses,
+        bound=bound,
     )
 
     outputs = model(attacked_img)
@@ -106,19 +202,22 @@ def main(model: nn.Module) -> None:
         (attacked_img.squeeze(0) * 255).to(torch.uint8),
         boxes.detach(),
         labels,
+        # Use these if you want to customize font & color
         # colors=["#0080FF" for _ in range(len(labels))],
         # font=f"{ASSETS_ROOT}/16020_FUTURAM.ttf",
         # font_size=24,
     )
     result = result / 255
 
-    save_image(attacked_img, f"{SAVE_ROOT}/attacked.png")
-    save_image(result, f"{SAVE_ROOT}/attacked_result.png")
-    save_image((attacked_img - img) ** 2, f"{SAVE_ROOT}/diff.png")
+    save_image(attacked_img, f"{save_path}/attacked.png")
+    save_image(result, f"{save_path}/attacked_result.png")
+    save_image((attacked_img - img) ** 2, f"{save_path}/diff.png")
 
 
 if __name__ == "__main__":
     model = fasterrcnn_resnet50_fpn_v2(
         weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT, box_score_thresh=0.9
     )
-    main(model)
+    for _, setting in SETTINGS.items():
+        print(f"Running {setting['save_path']}")
+        main(model, **setting)
